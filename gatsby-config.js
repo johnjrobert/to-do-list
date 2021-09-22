70a8fb00f226cab7db3dca12b9278069bb931604
johnjrobert <78804682+johnjrobert@users.noreply.github.com>
2021-09-22 16:49:26 -0400

    Fix?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~jrobert/todo-list",
     title: "To-Do-List",
   },
   plugins: [],
+  pathPrefix: "/~jrobert/todo-list",
 };
